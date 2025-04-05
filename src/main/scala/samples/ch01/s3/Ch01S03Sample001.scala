ff3f2a338d5b41827b31c7b79ef0f7994fa7afc0
Adam Bridge <adam.bridge@icapitalnetwork.com>
2025-04-04 20:34:33 -0400

    Chapter 1.3 code sample

---
+++ src/main/scala/samples/ch01/s3/Ch01S03Sample001.scala
@@ -0,0 +1,38 @@
+package samples.ch01.s3
+
+import scala.io.StdIn
+import zio.{ZIO, ZIOAppDefault}
+
+object Ch01S03Sample001a extends ZIOAppDefault {
+  val firstName =
+    ZIO.attempt(StdIn.readLine("What is your first name?"))
+
+  val lastName =
+    ZIO.attempt(StdIn.readLine("What is your last name"))
+
+  val fullName =
+    firstName.zipWith(lastName)((first, last) => s"$first $last")
+
+  val run = fullName
+}
+
+object Ch01S03Sample001b extends ZIOAppDefault {
+  val firstName =
+    ZIO.attempt(StdIn.readLine("What is your first name?"))
+
+  val lastName =
+    ZIO.attempt(StdIn.readLine("What is your last name"))
+
+  val fullName =
+    firstName.zipWith(lastName)((first, last) => s"$first $last")
+
+  def printLine(line: String) =
+    ZIO.attempt(println(line))
+
+  val printFullName =
+  for { n <- fullName
+    _ <- printLine(n)
+  } yield ()
+
+  val run = printFullName
+}
