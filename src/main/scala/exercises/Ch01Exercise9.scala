24f904934c5186808535e47268e1401f62936f16
Adam Bridge <adam.bridge@icapitalnetwork.com>
2025-07-14 17:10:52 -0400

    Chapter 1 exercise 9

---
+++ src/main/scala/exercises/Ch01Exercise9.scala
@@ -0,0 +1,26 @@
+package exercises
+
+
+object Ch01Exercise9 extends App {
+  //  9. Implement the orElse function in terms of the toy model of a ZIO effect. The
+  //  function should return an effect that tries the left-hand side, but if that effect fails,
+  //  it will fall back to the effect on the right-hand side.
+
+  def orElse[R, E1, E2, A](
+    self: ZIO[R, E1, A],
+    that: ZIO[R, E2, A]
+  ): ZIO[R, E2, A] = {
+    ZIO( r =>
+      self.run(r) match {
+        case Right(a) => Right(a)
+        case Left(e1) => that.run(r)
+      }
+    )
+  }
+
+  val z1 = ZIO(incrementUnlessNegative)
+  val z2 = ZIO(doubleUnlessOdd)
+  val zelse = orElse(z1, z2)
+  val result = zelse.run(-2)
+  println(s"result: $result")
+}
--- src/main/scala/exercises/package.scala
+++ src/main/scala/exercises/package.scala
@@ -4,9 +4,9 @@ package object exercises {
 
   def incrementUnlessNegative(x: Int) =
     if (x >= 0) Right(x + 1)
-    else Left("Nope")
+    else Left("Nope, no negatives allowed")
 
   def doubleUnlessOdd(x: Int) =
     if (x % 2 == 0) Right(2 * x)
-    else Left("Nein danke")
+    else Left("Nein danke, too odd")
 }
