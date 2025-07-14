c3e386b2b18fc5f80fc7a494dff059f4d655e982
Adam Bridge <adam.bridge@icapitalnetwork.com>
2025-07-14 18:05:39 -0400

    Everything is object!

---
+++ src/main/scala/exercises/Ch01Exercise03.scala
@@ -0,0 +1,40 @@
+package exercises
+
+import zio.ZIO
+
+object Ch01Exercise03 {
+  // 3. Using the flatMap method of ZIO effects, together with the readFileZio and
+  // writeFileZiofunctions that you wrote, implement a ZIO version of the function
+  // copyFile.
+
+  def copyFile(source: String, dest: String): Unit = {
+    val contents = readFile(source)
+    writeFile(dest, contents)
+  }
+
+  def copyFileZio(source: String, dest: String) =
+    readFileZio(source)
+      .flatMap(lines =>
+        writeFileZio(dest, lines)
+      )
+
+
+
+  def readFile(file: String): String = {
+    val source = scala.io.Source.fromFile(file)
+
+    try source.getLines().mkString
+    finally source.close()
+  }
+
+  def readFileZio(file: String) = ZIO.attempt(readFile(file))
+
+  def writeFile(file: String, text: String): Unit = {
+    import java.io._
+    val pw = new PrintWriter(new File(file))
+    try pw.write(text)
+    finally pw.close
+  }
+
+  def writeFileZio(file: String, text: String) = ZIO.attempt(writeFile(file, text))
+}
---
+++ src/main/scala/exercises/Ch01Exercise04.scala
@@ -0,0 +1,21 @@
+package exercises
+
+import zio.ZIO
+
+object Ch01Exercise04 {
+  // 4. Rewrite the following ZIO code that uses flatMapinto a for comprehension.
+
+  def printLine(line: String) = ZIO.attempt(println(line))
+
+  val readLine = ZIO.attempt(scala.io.StdIn.readLine())
+
+  printLine("What is your name?").flatMap(_ =>
+    readLine.flatMap(name => printLine(s"Hello, $name!"))
+  )
+
+  for {
+    _ <- printLine("What is your name?")
+    name <- readLine
+    _ <- printLine(s"Hello, $name!")
+  } yield ()
+}
