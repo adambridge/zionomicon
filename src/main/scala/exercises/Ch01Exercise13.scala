1775dbdf6a686d3c98e9a5b9077be87123787226
Adam Bridge <adam.bridge@icapitalnetwork.com>
2025-07-14 19:03:33 -0400

    Chapter 1 exercise 13

---
+++ src/main/scala/exercises/Ch01Exercise13.scala
@@ -0,0 +1,18 @@
+package exercises
+
+import zio.{ZIO, ZIOAppDefault}
+
+object Ch01Exercise13 extends ZIOAppDefault {
+  //  13. Using ZIO.succeed, convert the following procedural function into a ZIO function:
+  def currentTime(): Long = java.lang.System.currentTimeMillis()
+
+  lazy val currentTimeZIO: ZIO[Any, Nothing, Long] =
+    ZIO.succeed(currentTime())
+
+  val run = {
+    for {
+      result <- currentTimeZIO
+    } yield println(s"result: $result")
+  }
+}
+
