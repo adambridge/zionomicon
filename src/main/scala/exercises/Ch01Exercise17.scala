96dde8343d60ceed25c26da381ed935749207b74
Adam Bridge <adam.bridge@icapitalnetwork.com>
2025-08-10 14:17:09 -0400

    Revise chapter 1 exercise 16

---
+++ src/main/scala/exercises/Ch01Exercise17.scala
@@ -0,0 +1,35 @@
+package exercises
+
+import zio.{ZIO, ZIOAppDefault}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+object Ch01Exercise17 extends ZIOAppDefault {
+  // 16. Using ZIO.fromFuture, convert the following code to ZIO:
+
+  trait Query
+  trait Result
+
+  final case class ExampleQuery(query: String) extends Query
+  final case class ExampleResult(result: String) extends Result
+
+  def doQuery(query: Query)(implicit
+    ec: ExecutionContext
+  ): Future[Result] =
+    query match {
+      case ExampleQuery("valid-query") => Future.successful(ExampleResult("result"))
+      case ExampleQuery(_) => Future.failed(new Throwable("query-failed"))
+    }
+
+  def doQueryZio(query: Query): ZIO[Any, Throwable, Result] =
+    ZIO.fromFuture(ec => doQuery(query)(ec))
+
+  val run = {
+    for {
+      result <- doQueryZio(ExampleQuery("valid-query"))
+//      Uncomment the next line to test with an invalid key
+//      result <- doQueryZio(ExampleQuery("invalid-query"))
+    } yield println(s"result: $result")
+  }
+}
+
