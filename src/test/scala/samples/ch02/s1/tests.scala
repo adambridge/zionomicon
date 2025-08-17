9dc6aa8aefea16de5b08642255160636406fd41e
Adam Bridge <adam.bridge@icapitalnetwork.com>
2025-08-17 11:16:41 -0400

    Chapter 2.1 test samples

---
+++ src/test/scala/samples/ch02/s1/tests.scala
@@ -0,0 +1,33 @@
+package samples.ch02.s1
+
+
+import zio.test._
+import zio.test.Assertion._
+import zio.ZIO
+
+object ExampleSpec extends ZIOSpecDefault {
+
+  def spec = suite("ExampleSpec")(
+    test("addition works") {
+      assert(1 + 1)(equalTo(2))
+    },
+    test("ZIO.succeed succeeds with specified value") {
+      assertZIO(ZIO.succeed(1 + 1))(equalTo(2))
+    },
+    test("testing an effect using map operator") {
+      ZIO.succeed(1 + 1).map(n => assert(n)(equalTo(2)))
+    },
+    test("testing an effect using a for comprehension") {
+      for {
+        n <- ZIO.succeed(1 + 1)
+      } yield assert(n)(equalTo(2))
+    },
+    test("and") {
+      for {
+        x <- ZIO.succeed(1)
+        y <- ZIO.succeed(2)
+      } yield assert(x)(equalTo(1)) &&
+        assert(y)(equalTo(2))
+    }
+  )
+}
