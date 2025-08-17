a35d884f4dbf8a29c564216f574769bb3fb8cf2b
Adam Bridge <adam.bridge@icapitalnetwork.com>
2025-08-17 16:52:56 -0400

    Samples 2.4 Test Aspects

---
+++ src/test/scala/samples/ch02/s4/tests.scala
@@ -0,0 +1,15 @@
+package samples.ch02.s4
+
+import zio.test.Assertion._
+import zio.test.TestAspect._
+import zio.test._
+import zio.ZIO
+
+object ExampleSpec extends ZIOSpecDefault {
+
+  def spec = suite("ExampleSpec")(
+    test("this test will be repeated to ensure it is stable") {
+      assertZIO(ZIO.succeed(1 + 1))(equalTo(2))
+    } @@ nonFlaky
+  )
+}
