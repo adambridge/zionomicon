2376a0fbb32fb32eb2a810047980f297e43dac49
Adam Bridge <adam.bridge@icapitalnetwork.com>
2025-12-29 16:35:16 -0500

    Use repeatN in rate limiter

--- src/main/scala/exercises/ch02/ex3/RateLimiter.scala
+++ src/main/scala/exercises/ch02/ex3/RateLimiter.scala
@@ -15,29 +15,35 @@ class RateLimiter(N: Int) {
 
   private def nEventsLastMinute = for {
     timeNow <- now
-    count <- ZIO.attempt(eventTimes.takeWhile(lessThanOneMinuteOld(timeNow)(_)).size)
+    count <- ZIO.attempt(
+      eventTimes.takeWhile(lessThanOneMinuteOld(timeNow)(_)).size
+    )
   } yield count
 
-  def execute = for {
+  def execute: ZIO[Any, Throwable, Boolean] = for {
     n <- nEventsLastMinute
     allowed <- ZIO.succeed(n < N)
     timeNow <- now
     _ <- ZIO.attempt {
-      if (allowed) eventTimes = timeNow :: eventTimes.takeWhile(lessThanOneMinuteOld(timeNow)(_))
-      else if (allowed) eventTimes = eventTimes.takeWhile(lessThanOneMinuteOld(timeNow)(_))
+      if (allowed)
+        eventTimes =
+          timeNow :: eventTimes.takeWhile(lessThanOneMinuteOld(timeNow)(_))
+      else if (allowed)
+        eventTimes = eventTimes.takeWhile(lessThanOneMinuteOld(timeNow)(_))
     }
   } yield allowed
 }
 
 object RateLimiterApp extends ZIOAppDefault {
+
   val rateLimiter = new RateLimiter(2)
-  def run = {
-    ZIO.foreach(1 to 10) { n =>
-      for {
-        succeeded <- rateLimiter.execute.delay(1.second)
-        _ <- if (succeeded) Console.printLine(s"$n: succeess")
-             else Console.printLine(s"$n: fail")
-      } yield ZIO.succeed()
-    }
+
+  def run: ZIO[Any, Throwable, ZIO[Any, Nothing, Unit]] = {
+    (for {
+      succeeded <- rateLimiter.execute.delay(1.second)
+      _ <-
+        if (succeeded) Console.printLine("success")
+        else Console.printLine(s"fail")
+    } yield ZIO.succeed()).repeatN(10)
   }
 }
