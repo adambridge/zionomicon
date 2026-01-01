8ee33305c9cfe8d7d2b41b8a6c6efca62cec6c84
Adam Bridge <adam.bridge@icapitalnetwork.com>
2026-01-01 16:54:21 -0500

    Add ListReverser

---
+++ src/main/scala/exercises/ch02/ex4/ListReverser.scala
@@ -0,0 +1,25 @@
+package exercises.ch02.ex4
+
+import zio.{Console, Task, ZIO, ZIOAppDefault}
+
+import scala.annotation.tailrec
+
+object ListReverser {
+  def reverse[T](l: List[T]): Task[List[T]] =
+    ZIO.attempt(accumulateReverse(l, List[T]()))
+
+  @tailrec
+  private def accumulateReverse[T](l: List[T], acc: List[T]) :List[T] = {
+    l match {
+      case Nil => acc
+      case h :: t => accumulateReverse(t, h :: acc)
+    }
+  }
+}
+
+object ListReverserApp extends ZIOAppDefault {
+  def run: ZIO[Any, Throwable, Unit] = for {
+    reversed <- ListReverser.reverse(List("a", "l", "o", "h"))
+    _ <- Console.printLine(reversed)
+  } yield ()
+}
