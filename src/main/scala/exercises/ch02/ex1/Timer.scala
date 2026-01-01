cadb36c085ded64899219e4914a170a5e29736cc
Adam Bridge <adam.bridge@icapitalnetwork.com>
2026-01-01 13:47:19 -0500

    Fix Timer and TimerSpec

--- src/main/scala/exercises/ch02/ex1/Timer.scala
+++ src/main/scala/exercises/ch02/ex1/Timer.scala
@@ -7,7 +7,7 @@ import java.io.IOException
 object Timer extends ZIOAppDefault {
   def countdown(from: Int): IO[IOException, Unit] = {
       if (from > 0 ) Console.printLine(s"$from").delay(1.seconds) *> countdown(from - 1)
-      else Console.printLine(s"0")
+      else Console.printLine(s"0").delay(1.seconds)
   }
 
   def run: IO[IOException, Unit] = countdown(10)
--- src/test/scala/exercises/ch02/TimerSpec.scala
+++ src/test/scala/exercises/ch02/TimerSpec.scala
@@ -3,14 +3,16 @@ package exercises.ch02
 import exercises.ch02.ex1.Timer
 import zio.{ZIO, durationInt}
 import zio.test.Assertion.equalTo
-import zio.test.{TestClock, TestConsole, ZIOSpecDefault, assert, assertCompletes}
+import zio.test.{Spec, TestClock, TestConsole, ZIOSpecDefault, assert, assertCompletes}
+
+import java.io.IOException
 
 object TimerSpec extends ZIOSpecDefault {
-  def spec = suite("TimerSpec")(
+  def spec: Spec[Any, IOException] = suite("TimerSpec")(
     test("countdown prints 5 second countdown (grug brained test)") {
       for {
-        fiber <- Timer.countdown.fork
-        _ <- TestClock.adjust(1.second)
+        fiber <- Timer.countdown(5).fork
+        _ <- TestClock.adjust(1100.millis)
         one <- TestConsole.output
         _ <- TestClock.adjust(1.second)
         two <- TestConsole.output
@@ -21,32 +23,32 @@ object TimerSpec extends ZIOSpecDefault {
         _ <- TestClock.adjust(1.second)
         five <- TestConsole.output
         _ <- TestClock.adjust(1.second)
+        six <- TestConsole.output
         _ <- fiber.join
       } yield {
-        assertCompletes
-        assert(one)(equalTo(Vector("1\n")))
-        assert(two)(equalTo(Vector("1\n", "2\n")))
-        assert(three)(equalTo(Vector("1\n", "2\n", "3\n")))
-        assert(four)(equalTo(Vector("1\n", "2\n", "3\n", "4\n")))
-        assert(five)(equalTo(Vector("1\n", "2\n", "3\n", "4\n", "5\n")))
+        assertCompletes &&
+        assert(one)(equalTo(Vector("5\n"))) &&
+        assert(two)(equalTo(Vector("5\n", "4\n"))) &&
+        assert(three)(equalTo(Vector("5\n", "4\n", "3\n"))) &&
+        assert(four)(equalTo(Vector("5\n", "4\n", "3\n", "2\n"))) &&
+        assert(five)(equalTo(Vector("5\n", "4\n", "3\n", "2\n", "1\n"))) &&
+        assert(six)(equalTo(Vector("5\n", "4\n", "3\n", "2\n", "1\n", "0\n")))
       }
     },
-
     test("countdown prints 5 second countdown (big brain version)") {
       for {
-        fiber <- Timer.countdown.fork
-        outputs <- ZIO.foreach(1 to 5) { _ =>
-          TestClock.adjust(1.second) *> TestConsole.output
-        } // Spurious error "No implicits found for parameter bf: zio.BuildFrom ..." ?
-        _ <- TestClock.adjust(1.second)
+        fiber <- Timer.countdown(5).fork
+        outputs <- ZIO.collectAll(
+          List.fill(6)(TestConsole.output <* TestClock.adjust(1.second))
+        )
         _ <- fiber.join
       } yield {
-        assertCompletes
-        assert(outputs.zipWithIndex.forall { case (out, i) =>
-          out == (1 to (i + 1)).map(n => s"$n\n").toVector
-        })(equalTo(true))
+        assertCompletes &&
+          outputs.zipWithIndex.map { case (out, i) =>
+            val expected = (5 until 5 - i by -1).map(n => s"$n\n").toVector
+            assert(out)(equalTo(expected))
+          }.reduce(_ && _)
       }
-
     }
   )
 }
